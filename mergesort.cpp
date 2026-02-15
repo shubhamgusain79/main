a50556591c09c826238542f1bbebe88a01253a17
shubhamgusain79 <shubhamgusain79@gmail.com>
2026-02-15 20:11:59 +0530

    Add files via upload

---
+++ mergesort.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void merge(vector<int> &arr, int low, int mid, int high) {
+    int i = low;
+    int j = mid + 1;
+    vector<int> temp;
+
+    while (i <= mid && j <= high) {
+        if (arr[i] <= arr[j]) {
+            temp.push_back(arr[i]);
+            i++;
+        } else {
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+
+    while (i <= mid) {
+        temp.push_back(arr[i]);
+        i++;
+    }
+
+    while (j <= high) {
+        temp.push_back(arr[j]);
+        j++;
+    }
+
+    for (int k = low; k <= high; k++) {
+        arr[k] = temp[k - low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high) {
+    if (low < high) {
+        int mid = low + (high - low) / 2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array:\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
